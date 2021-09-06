127fed0f9ffd6635accb5eb18d5a3ca15dde0071
wtiinfo <wanbrwan@gmail.com>
2021-09-06 20:09:31 -0300

    Alterado dados estáticos de teste

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -21,53 +21,32 @@ const DataTable = () => {
                     <td>15017.00</td>
                 </tr>
                 <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>13/05/2021</td>
+                    <td>Bruce Wayne</td>
+                    <td>32</td>
+                    <td>20</td>
+                    <td>24517.00</td>
                 </tr>
                 <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>12/03/2021</td>
+                    <td>Clark Kent</td>
+                    <td>44</td>
+                    <td>26</td>
+                    <td>47047.00</td>
                 </tr>
                 <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>01/01/2021</td>
+                    <td>Diana</td>
+                    <td>33</td>
+                    <td>55</td>
+                    <td>85078.00</td>
                 </tr>
                 <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
+                    <td>25/10/2021</td>
+                    <td>Hal Jordan</td>
+                    <td>31</td>
+                    <td>20</td>
+                    <td>19027.60</td>
                 </tr>
             </tbody>
         </table>
