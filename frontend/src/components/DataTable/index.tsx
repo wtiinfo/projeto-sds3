9ef7cf4ae15c7f5daaacdaeff897f394866520d5
wtiinfo <wanbrwan@gmail.com>
2021-09-11 20:46:01 -0300

    DataTable integrada

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,5 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
 
 const DataTable = () => {
+
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`).then(response => {
+            setPage(response.data);
+        });
+    }, []);
+
     return (
         <div className="table-responsive">
         <table className="table table-striped table-sm">
@@ -13,41 +33,18 @@ const DataTable = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-                <tr>
-                    <td>13/05/2021</td>
-                    <td>Bruce Wayne</td>
-                    <td>32</td>
-                    <td>20</td>
-                    <td>24517.00</td>
-                </tr>
-                <tr>
-                    <td>12/03/2021</td>
-                    <td>Clark Kent</td>
-                    <td>44</td>
-                    <td>26</td>
-                    <td>47047.00</td>
-                </tr>
-                <tr>
-                    <td>01/01/2021</td>
-                    <td>Diana</td>
-                    <td>33</td>
-                    <td>55</td>
-                    <td>85078.00</td>
-                </tr>
-                <tr>
-                    <td>25/10/2021</td>
-                    <td>Hal Jordan</td>
-                    <td>31</td>
-                    <td>20</td>
-                    <td>19027.60</td>
-                </tr>
+                {
+                    page.content?.map(x => (
+                        <tr key={x.id}>
+                            <td>{formatLocalDate(x.date, "dd/MM/yyyy")}</td>
+                            <td>{x.seller.name}</td>
+                            <td>{x.visited}</td>
+                            <td>{x.deals}</td>
+                            <td>{x.amount.toFixed(2)}</td>
+                        </tr>
+                    ))
+                }
+               
             </tbody>
         </table>
     </div>
